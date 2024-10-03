2023738aac7fd985341e4ec0150227ec61c2ed13
AinebyonaAudrey <audreyainebyona@gmail.com>
2024-10-03 06:26:32 +0300

    stage 2

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % escape(username)
 
-def hello():
-    return "HELLO WORLD"
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath %s' % escape(subpath)
